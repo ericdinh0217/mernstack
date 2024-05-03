e66585f809047fce0b0b4dda085d1aba46c715d2
ninjaman0217 <eric.dinh.217@gmail.com>
2024-05-03 10:28:43 -0700

    webpack setup

---
+++ ES6/frontend.js
@@ -0,0 +1,27 @@
+//welcome.html
+
+//localhost:9000/welcome
+
+// write code in javascript using es6, jsx, react, + other libraries to create html page on the fly
+// page1.js
+// page2.js
+// page3.js, etc... 
+
+// some css
+// some images
+
+// tasks to do
+// 1. need to create a bundle of js pages
+// 2. need to indexing (bootstrapping) of component
+// 3. injecting css, images etc in .js file
+// 4. transpilation - converting code in es6, jsx, reduc, react, axios, ... etc into vanilla javascript format
+    // this requires to use third party libraries - babel
+    // those third party modules are termed as loaders
+// 5. minification - removing extra spaces, setting up logic corrections, commented code for production release
+    // this requires to use third party libraries
+// 6. providing the web server to host the fron application
+// 7. images conversion - into base64
+
+// bundler, minification, etc... 
+
+// 250kb converts to base64 encoded value => ./public/250kb.jpeg ==> #adas#sadada#asdqmk#aasqi#
