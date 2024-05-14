d671d71e190f1a3403c897218a3b0dc0b4ae7a18
ninjaman0217 <eric.dinh.217@gmail.com>
2024-05-14 09:35:13 -0700

    react life cycle

--- ReactApp/src/app/Common/HomeComponent.js
+++ ReactApp/src/app/Common/HomeComponent.js
@@ -1,15 +1,140 @@
-import React from "react";
+import React, { Component, PureComponent } from "react";
 
-let Home = (props)=>{
-    return(
-        <>
-            <h2>Home Component</h2>
-            <h5>{props.parentName}</h5>
-        </>
-    )
+//this component implements shouldComponentUpdate in itself to reduce the render on state change
+export default class Home extends PureComponent {
+
+//export default class Home extends Component {
+
+    //creation life cycle method
+    constructor(props){
+
+        super(props)
+
+        this.state = {
+            age : 17,
+            userName : "Default"
+        }
+
+        this.incrementAgeLoop = null;
+        this.incrementAgeVal = 17;
+        this.incrementAge();
+    }
+
+    incrementAge = ()=>{
+
+        // this.incrementAgeLoop = setInterval(()=>{ //continous loop
+        //     this.incrementAgeVal++
+        //     this.setState({
+        //         age : this.incrementAgeVal
+        //     })
+
+        //     console.log(this.incrementAgeVal)
+        // }, 2000) //runs every  2 seconds forever - unless cleared
+
+        // setTimeout(()=>{
+        //     clearInterval(this.incrementAgeLoop)
+        // },5000)
+    }
+
+    //html is rendered on browser, executes only after the first render
+    componentDidMount(){
+        //we can access the html and make calls to server API here to pull the data
+    }
+
+    //destruction life cycle method
+    componentWillUnmount(){
+        //any api subsriptions, loops should be stopped here as this data may create mess
+        console.log("componentWillUnmount - is called")
+        //clearInterval(this.incrementAgeLoop)
+    }
+
+    //evt -is js object which contain information about the control which invoked this event
+    onTextChange = (evt)=>{
+        let element = evt.target
+        let value = element.value;
+
+        this.setState({
+            userName : value
+        })
+        console.log(value)
+        
+        evt.preventDefault() //will explain in detail
+    }
+
+    updateName = (evt)=>{
+        console.log("Updating the nameto age!!")
+        this.setState({
+            age : this.state.userName
+        })
+
+        evt.preventDefault()
+    }
+
+    // do not implement this if in pure component
+    //update life cycle methods
+    // shouldComponentUpdate(nextPops, nextState){
+    //     console.log("nextPops ", nextPops)
+    //     console.log("nextState ", nextState)
+
+    //     if (this.state.age == nextState.userName ) {
+    //         return false // should not call render method to create virtual dom
+    //     } else {
+    //         return true // keep calling render method     
+    //     }
+    // }
+
+    getSnapshotBeforeUpdate(prevState, prevProps){
+        console.log("getSnapshotBeforeUpdate");
+        console.log("prevState", prevState);
+        console.log("prevProps", prevProps);
+        return {
+            prevState,
+            prevProps
+        }
+    }
+
+    componentDidUpdate(prevState, prevProps){
+        console.log("componentDidUpdate");
+        console.log("prevState",prevState);
+        console.log("prevProps", prevProps);
+
+        // this.setState({
+        //     uState : prevState.uState
+        // })
+    }
+
+
+    //create and update the virtual DOM
+    render(){
+        console.log("render method called")
+        return(
+            <div className="col-md-12">
+                <h1>Home Component</h1>
+                <h2>User Age is {this.state.age} </h2>
+
+                {/* controlled way of creating component - state directly coupled with changes*/}
+                <div className="form col-md-12">
+                    <div className="form-control">
+                        <div className="col-md-3">
+                            <b>User Name</b>
+                        </div>
+                        <div className="col-md-7">
+                            <input type="text" className="form-control textbox" value={this.state.userName}
+                                placeholder="Please provide user name" onChange={this.onTextChange}></input>
+                        </div>
+
+                        <div className="col-md-3">
+                        <button className={"form-control btn btn-primary col-md-1"} 
+                            onClick={this.updateName} 
+                        >Update Name to Age</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
 }
-
-export default Home;
 
 //class component 
 //life cycle methods
