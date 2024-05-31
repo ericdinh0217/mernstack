2f5ec667badff913f3a0a58fa577018016a9a32c
ninjaman0217 <eric.dinh.217@gmail.com>
2024-05-31 09:30:08 -0700

    5/31/2024

--- ReactApp/src/app/Common/FooterComponent.js
+++ ReactApp/src/app/Common/FooterComponent.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 let Footer = ()=>{
+    //Javascript Like XML structure (not html, not xml - but js)
     return(
         <>
-            <h2>Footer Component</h2>
-            <hr/>
+            <div className="footer">
+                © Copyright 2023 All rights reserved. &nbsp;|&nbsp; 
+                <a href="https://www.synergisticit.com/" target="_blank">SynergisticIT</a> &nbsp;|&nbsp; 
+                <a href="http://www.synergisticit.com/sitemap.xml" target="_blank">Sitemap</a>
+            </div>
         </>
     )
 }
---
+++ ReactApp/src/app/Common/ProjectEnchancement.js
@@ -0,0 +1,25 @@
+
+// 30-05-2024 - Create Product and Display Them
+
+// Create a product component using functional component and hooks (try using useRef as react hook and implement accordingly)
+// Create a form to allow user to submit Product Details - name, price, desc, rating
+// Create an action method to add the detail to database using a server api
+// Server Side - Create product router and api to save the product using productdatamodel
+// Everything should be done in continuation with shopping cart project
+
+
+//Task 28th May2024
+
+// Hobby - Additional Task
+//1. Create a component with name Hobby, add a textbox to insert hobbyname into it and save it to the database using SaveAPI
+//2. Create an action to save the hobby, can use useractions file and userrouter file for the same
+
+//Fetch the saved hobbies and show them in Login Page so that user can select any hobby and update the user info
+
+
+//- StudentReact > Component, Route, NavLink,Action, Reducer,
+//StudentName, Age, Address, etc
+
+// -StudentAPI >
+// StudentDataModel, Router/API/Endpoints to save and fetch students
+// -ExpressApp for students and do mounting
